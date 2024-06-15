9d2b526d157a2b674b6b5fe28b2c906170f5e418
AndresPJ09 <130188551+AndresPJ09@users.noreply.github.com>
2024-06-15 17:53:27 -0500

    Frontend Solo interfaces

---
+++ FRONT/Frontend Prueba/js/BusquedaVuelo.js
@@ -0,0 +1,14 @@
+// script.js
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aquí puedes agregar el código para enviar la solicitud de búsqueda de vuelos al backend
+    // y manejar la respuesta para mostrar los vuelos disponibles.
+});
+
+document.getElementById('reserve-button').addEventListener('click', function() {
+    // Aquí puedes agregar el código para reservar el vuelo seleccionado.
+});
+
+document.getElementById('exit-button').addEventListener('click', function() {
+    // Aquí puedes agregar el código para salir o limpiar el formulario.
+});
